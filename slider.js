7b348cc7c1108a80a5e98b11b384fd86cc6794e4
Terence Liu <liu_terence@hotmail.com>
2019-02-12 02:12:43 -0800

    Adding Shop Now to Home Commit

--- slider.js
+++ slider.js
@@ -13,14 +13,14 @@ $(document).ready(function() {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-          dots: true
+          dots: true,
         }
       },
       {
         breakpoint: 540,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
         }
       },
       // You can unslick at a given breakpoint now by adding:
