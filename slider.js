83feec19367c91c25f1c67998d6017cb563feece
Terence Liu <liu_terence@hotmail.com>
2019-02-11 21:48:34 -0800

    Editing Alignment of Carousel Commit

--- slider.js
+++ slider.js
@@ -8,28 +8,21 @@ $(document).ready(function() {
     autoplaySpeed: 2000,
     speed: 300,
     responsive: [{
-        breakpoint: 1024,
+        breakpoint: 840,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 2,
+          slidesToScroll: 1,
           infinite: true,
           dots: true
         }
       },
       {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
+        breakpoint: 540,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1
         }
-      }
+      },
       // You can unslick at a given breakpoint now by adding:
       // settings: "unslick"
       // instead of a settings object
